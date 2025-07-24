77c666c273662da2b2d7aafca03af85057d883a5
jproven <provenciozamorajuan@gmail.com>
2025-07-24 20:12:23 +0200

    Add 404 Not Found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { ManageComponent } from './manage/manage.component';
+import { Page404Component } from './page404/page404.component';
 
 export const routes: Routes = [
      {path: '', redirectTo: '/home', pathMatch: 'full'},
      {path: 'home', component: HomeComponent},
      {path: 'search', component: SearchComponent},
-     {path: 'manage', component: ManageComponent}
+     {path: 'manage', component: ManageComponent},
+     {path: "**", component: Page404Component}
 ];
