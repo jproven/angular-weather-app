7cfb708c4b4dd920bcfdaef4ff9c4d9da2bb163a
jproven <provenciozamorajuan@gmail.com>
2025-07-24 20:01:29 +0200

    Feat(manage): enable real-time filtering of favorite cities while typing

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -2,26 +2,41 @@ import { Component } from '@angular/core';
 import { CityCardComponent } from '../components/city-card/city-card.component';
 import { CitiesService } from '../cities.service';
 import { DataApiService } from '../data-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage',
-  imports: [CityCardComponent],
+  imports: [FormsModule, CityCardComponent],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css',
 })
 export class ManageComponent {
-  
-  constructor(private citiesService: CitiesService,
-              private dataApiService: DataApiService) 
-                { this.citiesService.getCities().forEach( c => {
-                    this.dataApiService.updateById(c.id).subscribe( 
-                      json => {
-                        this.citiesService.updateCity(json)
-                      }
-                    )
-                })}
+  searchTerm: string = '';
+  filteredCities: any[] = [];
+
+  constructor(
+    private citiesService: CitiesService,
+    private dataApiService: DataApiService
+  ) {
+    this.citiesService.getCities().forEach((c) => {
+      this.dataApiService.updateById(c.id).subscribe((json) => {
+        this.citiesService.updateCity(json);
+      });
+    });
+  }
 
   getCities() {
-    return this.citiesService.getCities();  
+    if (this.searchTerm.trim() === '') {
+      return this.citiesService.getCities();
+    } else {
+      return this.filteredCities;
+    }
+  }
+
+  searchCity() {
+    const term = this.searchTerm.trim().toLowerCase();
+    this.filteredCities = this.citiesService
+      .getCities()
+      .filter((city) => city.name.toLowerCase().includes(term));
   }
 }
